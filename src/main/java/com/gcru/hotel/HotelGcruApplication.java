d84ea390e8380a2e2eb9160ac1537936af543d7d
gian <gian_ru350@hotmail.com>
2020-10-21 14:13:37 -0500

    Creación del proyecto

---
+++ src/main/java/com/gcru/hotel/HotelGcruApplication.java
@@ -0,0 +1,15 @@
+package com.gcru.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelGcruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelGcruApplication.class, args);
+		
+		// hola mundo
+	}
+
+}
---
+++ src/test/java/com/gcru/hotel/HotelGcruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcru.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelGcruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
